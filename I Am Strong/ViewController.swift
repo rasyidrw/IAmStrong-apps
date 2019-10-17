10c3ea8e93fdb31f69f6e4c24ad4d59ba9dec302
rasyidrw <rasyidrespati.30@gmail.com>
2019-10-17 13:54:00 +0700

    first commit IamStrong apps

--- I Am Strong/ViewController.swift
+++ I Am Strong/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("I Am Strong music player UI")
     }
 
 

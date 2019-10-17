8bf8896e852025cc73ef42656a794f86c57f1bb6
rasyidrw <rasyidrespati.30@gmail.com>
2019-10-17 11:45:15 +0700

    Initial Commit

---
+++ I Am Strong/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Strong
+//
+//  Created by Rasyid Respati Wiriaatmaja on 17/10/19.
+//  Copyright © 2019 rasyidrw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
